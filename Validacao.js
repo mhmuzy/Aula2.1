1af4f8bded5e8ceee3f4bb86b8b021bab08ad0af
marcio henrique <mhmuzy@embratel.com.br>
2021-08-07 20:06:09 -0300

    first commit

---
+++ Validacao.js
@@ -0,0 +1,17 @@
+
+console.log("");
+console.log("***************************************************************")
+console.log("                   SERPRO TREINAMENTO:")
+console.log("***************************************************************");
+const num = 5;
+console.log("");
+console.log("                   RESULTADO DO PROCESSO:")
+console.log("------------------------------------------------------------------");
+console.log("");
+if (num == 5) {
+    console.log("O Nùmero é 5!")
+} else {
+    console.log("O Número não é 5!")
+}
+console.log("");
+console.log("------------------------------------------------------------------");
